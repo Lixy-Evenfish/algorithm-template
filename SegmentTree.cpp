6d5bf1aaef694d117df963b9e1659c4344343c4f
李欣悦QAQ <571780113@qq.com>
2025-09-24 15:43:25 +0800

    线段树补充功能

--- SegmentTree.cpp
+++ SegmentTree.cpp
@@ -3,28 +3,36 @@
 using namespace std;
 
 struct info {
+	//需要修改！！！
 	int l,r;
 	int sum;
+	int mi,mx;
 };
 info operator+ (const info &l,const info &r) {
+	//需要修改！！！
 	info a;
 	a.l = min(l.l,r.l),a.r = max(l.r,r.r);
 	a.sum = l.sum + r.sum;
+	a.mx = max(l.mx,r.mx);
+	a.mi = min(l.mi,r.mi);
 	return a;
 }
-struct segment_tree{
+struct Segtree{
 	const int len;
 	vector<info> seg;
 	vector<int> tag;
-	segment_tree(int n1) :len(n1),seg(4*n1+1),tag(4*n1+1) {}
+	Segtree(int n1) :len(n1),seg(4*n1+1),tag(4*n1+1) {}
 	void add(int np,int v)
 	{
+		//需要修改！！！
 		tag[np] += v;
+		seg[np].mx += v;
+		seg[np].mi -= v;
 		seg[np].sum += (seg[np].r-seg[np].l+1)*v;
 	}
 	void push(int np)
 	{
-		add(2*np,tag[np]),add(2*np+1,tag[np]);
+		add(np << 1,tag[np]),add(np << 1 | 1,tag[np]);
 		tag[np] = 0;
 	}
 	void pull(int id) {
@@ -39,7 +47,21 @@ struct segment_tree{
 			{
 				seg[np] = {l, r};
 				int mid = l + r >> 1;
-				self(self,np*2, l, mid), self(self,np*2 + 1, mid + 1, r);
+				self(self,np << 1, l, mid), self(self,np << 1 | 1, mid + 1, r);
+				pull(np);
+			}
+		};
+		build(build,1,0,len);
+	}
+	void init(){
+		auto build = [&](auto &&self,int np,int l,int r) -> void
+		{
+			if (l == r) seg[np] = {l, r, 0};
+			else
+			{
+				seg[np] = {l, r};
+				int mid = l + r >> 1;
+				self(self,np << 1, l, mid), self(self,np << 1 | 1, mid + 1, r);
 				pull(np);
 			}
 		};
@@ -49,14 +71,17 @@ struct segment_tree{
 	{
 		if(seg[np].l == seg[np].r)
 		{
+			//需要修改！！！
 			seg[np].sum += v;
+			seg[np].mx += v;
+			seg[np].mi -= v;
 		}
 		else
 		{
 			int mid = seg[np].l + seg[np].r >> 1;
 			push(np);
-			if(x <= mid) modify(2*np,x,v);
-			else modify(2*np+1,x,v);
+			if(x <= mid) modify(np << 1,x,v);
+			else modify(np << 1 | 1,x,v);
 			pull(np);
 		}
 	}
@@ -69,8 +94,8 @@ struct segment_tree{
 		else{
 			push(np);
 			int mid = seg[np].l + seg[np].r >> 1;
-			if(x <= mid) rangeadd(2*np,x,y,v);
-			if(y > mid) rangeadd(2*np+1,x,y,v);
+			if(x <= mid) rangeadd(np << 1,x,y,v);
+			if(y > mid) rangeadd(np << 1 | 1,x,y,v);
 			pull(np);
 		}
 	}
@@ -81,9 +106,9 @@ struct segment_tree{
 		int mid = seg[np].l + seg[np].r >> 1;
 		if(x <= mid && y > mid)
 		{
-			return query(np*2,x,y) + query(np*2+1,x,y);
+			return query(np << 1,x,y) + query(np << 1 | 1,x,y);
 		}
-		else if(x <= mid) return query(np*2,x,y);
-		else return query(np*2+1,x,y);
+		else if(x <= mid) return query(np << 1,x,y);
+		else return query(np << 1 | 1,x,y);
 	}
 };
