8806d2fa8144e1eae166220819987c8ded216b4b
李欣悦QAQ <571780113@qq.com>
2025-10-09 15:28:59 +0800

    增加数论分块

--- division block.cpp
+++ division block.cpp
@@ -1,27 +1,29 @@
 #include<bits/stdc++.h>
 #define int long long
 using namespace std;
-
-//上取整
-long long r;
-for(int l = 1; l <= n; l = r + 1)
-{
-    if(k/l == 0)
-    {
-        r = 8e18;       
-    }
-    else r = min(k/(k/l),n);
-}
-
-
-//下取整
-
-long long r;
-for(int l = a; l <= n ; l = r + 1)
-{
-	if((k-1)/l == 0)
+void solve(){
+	int n,k;
+	cin >> n >> k;
+	//上取整
+	long long r;
+	for(int l = 1; l <= n; l = r + 1)
 	{
-		r = 8e18;
+    	if(k/l == 0)
+    	{
+    	    r = 8e18;       
+    	}
+    	else r = min(k/(k/l),n);
 	}
-	else r = min((k-1)/((k-1)/l),n);
+
+
+	//下取整
+	long long r;
+	for(int l = a; l <= n ; l = r + 1)
+	{
+		if((k-1)/l == 0)
+		{
+			r = 8e18;
+		}
+		else r = min((k-1)/((k-1)/l),n);
+	}
 }
