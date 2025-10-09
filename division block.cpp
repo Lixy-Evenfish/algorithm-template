77d87b149c049801510cdf17e4b42b52590af86d
李欣悦QAQ <571780113@qq.com>
2025-10-09 15:22:49 +0800

    -1

--- division block.cpp
+++ division block.cpp
@@ -1,7 +1,8 @@
 #include<bits/stdc++.h>
 #define int long long
+using namespace std;
 
-//下取整
+//上取整
 long long r;
 for(int l = 1; l <= n; l = r + 1)
 {
@@ -15,11 +16,18 @@ for(int l = 1; l <= n; l = r + 1)
     *\
 }
 
-//上取整
-int r;
-for(int l = 1; l <= n; l = r + 1)
+
+//下取整
+
+long long r;
+for(int l = a; l <= n ; l = r + 1)
 {
-	if((m-1)/l == 0) r = 8e18;
-	else r = (m-1)/((m-1)/l);
-	ans = min(ans,((m+l-1)/l)*l - m + n - l);
+	if((k-1)/l == 0)
+	{
+		r = 8e18;
+	}
+	else r = min((k-1)/((k-1)/l),n);
+	\*
+        code
+    *\
 }
