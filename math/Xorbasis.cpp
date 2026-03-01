7d8fe3664b0f71f4a40d1dc6a614da980c50f082
李欣悦QWQ <571780113>
2026-03-01 12:12:30 +0800

    xorbasis

--- math/Xorbasis.cpp
+++ math/Xorbasis.cpp
@@ -3,19 +3,22 @@
 
 using namespace std;
 
+
+//高斯消元
 struct Xorbasis{
+	const int bit = 64;
 	int n,k;
-	vector<int> a;
+	vector<int> a,idx;
 	
 	void insert(int x){
 		a.push_back(x);
 	}
 	
-	//高斯消元求线性基
 	vector<int> work(){
 		n = a.size();
 		k = 0;
-		for(int i = 62; i >= 0; i --){
+		idx.resize(bit,-1);
+		for(int i = bit-2; i >= 0; i --){
 			for(int j = k; j < n; j ++){
 				if((a[j]>>i&1)){
 					swap(a[j],a[k]);
@@ -23,10 +26,14 @@ struct Xorbasis{
 				}
 			}
 			if(!(a[k]>>i&1)){
+				idx[i] = -1;
 				continue;
 			}
+			idx[i] = k;
 			for(int j = 0; j < n; j ++){
-				if(j != k && (a[j]>>i&1)) a[j] ^= a[k];
+				if(j != k && (a[j]>>i&1)){ 
+					a[j] ^= a[k];	
+				}
 			}
 			k ++;
 			if(k == n) break;
@@ -34,6 +41,14 @@ struct Xorbasis{
 		return a;
 	}
 	
+	
+	//判断第i位是否有主元
+	int getbit(int i){
+		if(idx[i] == -1)  return -1;
+		else return a[idx[i]];
+	}
+	
+		
 	//求最大
 	int getmax(){
 		int ret = 0;
@@ -59,5 +74,4 @@ struct Xorbasis{
 		}
 		return ret;
 	}
-
 };
