43add431f8da49ff9e23a3371c6bb13c017268d5
李欣悦QAQ <571780113@qq.com>
2026-03-13 17:43:19 +0800

    树状数组二分

--- data structure/Fenwick.cpp
+++ data structure/Fenwick.cpp
@@ -1,34 +1,46 @@
 #include<bits/stdc++.h>
 #define int long long
-using namespace std;
 
+using namespace std;
 struct Fenwick{
-    const int len;
-    vector<int> a;
-    Fenwick(int n1) : len(n1),a(len + 1,0){}
-    #define lowbit(x) ((x) & (-x))
-    void init(vector<int> &b)
-    {
-        for(int i = 1; i <= len; i ++)
-        {
-            a[i] += b[i];
-            int j = i + lowbit(i);
-            if(j <= len) a[j] += a[i];
-        }
-    }
-    void add(int x,int c)
-    {
-        for(int i = x; i <= len; i += lowbit(i))
-            a[i] += c;
-    }
-    int sum(int x)
-    {
-        int ret = 0;
-        for(int i = x; i; i -= lowbit(i)) ret += a[i];
-        return ret;
-    }
-    int sum(int l,int r)
-    {
-        return sum(r) - sum(l - 1);
-    }
+	const int len;
+	vector<int> a;
+	Fenwick(int n1) : len(n1),a(len + 3){}
+#define lowbit(x) ((x) & (-x))
+	void init(vector<int> &b)
+	{
+		for(int i = 1; i <= len; i ++)
+		{
+			a[i] += b[i];
+			int j = i + lowbit(i);
+			if(j <= len) a[j] += a[i];
+		}
+	}
+	void add(int x,int c)
+	{
+		for(int i = x; i <= len; i += lowbit(i))
+			a[i] += c;
+	}
+	int sum(int x)
+	{
+		int ret = 0;
+		for(int i = x; i; i -= lowbit(i)) ret += a[i];
+		return ret;
+	}
+	int sum(int l,int r)
+	{
+		return sum(r) - sum(l - 1);
+	}
+	
+	//返回sum <= k的最靠右位置
+	int select(int k){
+		int x = 0,cur = 0;
+		for(int i = 1 << __lg(len); i ; i /= 2){
+			if(x + i <= len && cur + a[x+i] <= k){
+				cur += a[x + i];
+				x += i;
+			}
+		}
+		return x;
+	}
 };
